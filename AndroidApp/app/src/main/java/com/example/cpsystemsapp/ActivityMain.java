9a4e3d8f5ae3a8c4073d274c1a787f34912778c0
zanplahuta <zan.plahuta@gmail.com>
2021-05-16 20:41:58 +0200

    Dodan register in login

--- AndroidApp/app/src/main/java/com/example/cpsystemsapp/ActivityMain.java
+++ AndroidApp/app/src/main/java/com/example/cpsystemsapp/ActivityMain.java
@@ -2,7 +2,16 @@ package com.example.cpsystemsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
 
 public class ActivityMain extends AppCompatActivity {
 
@@ -10,5 +19,38 @@ public class ActivityMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragment(new LoginFragment());
+        pagerAdapter.addFragment(new RegisterFragment());
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    class AuthenticationPagerAdapter extends FragmentPagerAdapter {
+        private ArrayList<Fragment> fragmentList = new ArrayList<>();
+
+        public AuthenticationPagerAdapter(FragmentManager fm) {
+            super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return fragmentList.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        void addFragment(Fragment fragment) {
+            fragmentList.add(fragment);
+        }
+    }
+    public void onClickOpentrack(View view) {
+        Intent i = new Intent(getBaseContext(), ActivityTrack.class);
+        startActivity(i);
     }
 }
---
+++ AndroidApp/app/src/main/java/com/example/cpsystemsapp/ActivityTrack.java
@@ -0,0 +1,14 @@
+package com.example.cpsystemsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityTrack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track);
+    }
+}
