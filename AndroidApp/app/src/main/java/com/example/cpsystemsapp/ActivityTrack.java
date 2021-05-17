1b7f7785081e359a8609a4215d538b1308ceeee0
zanplahuta <zan.plahuta@gmail.com>
2021-05-17 15:03:21 +0200

    Popravki
    Popravki v kodi

--- AndroidApp/app/src/main/java/com/example/cpsystemsapp/ActivityTrack.java
+++ AndroidApp/app/src/main/java/com/example/cpsystemsapp/ActivityTrack.java
@@ -1,14 +1,37 @@
 package com.example.cpsystemsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
+import android.os.Vibrator;
 
-public class ActivityTrack extends AppCompatActivity {
+public class ActivityTrack extends AppCompatActivity implements SensorEventListener {
+    protected MyApplication app;
+    Vibrator vibrator;
+    Sensor acelerometer;
+    Sensor gyroscope;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track);
+
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        app = (MyApplication) getApplication();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
---
+++ AndroidApp/app/src/main/java/com/example/cpsystemsapp/MyApplication.java
@@ -0,0 +1,6 @@
+package com.example.cpsystemsapp;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+}
