01c681ef5df5b672c078ffec75e7883939b43681
zanplahuta <zan.plahuta@gmail.com>
2021-06-01 12:11:26 +0200

    Zajem in shranjevanje slike

--- AndroidApp/app/src/main/java/com/example/cpsystemsapp/ActivityMenu.java
+++ AndroidApp/app/src/main/java/com/example/cpsystemsapp/ActivityMenu.java
@@ -1,21 +1,53 @@
 package com.example.cpsystemsapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class ActivityMenu extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+import static androidx.test.InstrumentationRegistry.getContext;
+
+public class ActivityMenu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
+
     private DrawerLayout drawer;
+    ImageView imageViewCamera;
+    ImageButton buttonSend, buttonCapture;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        imageViewCamera = findViewById(R.id.imageViewCamera);
+        buttonSend = findViewById(R.id.buttonSend);
+        buttonCapture = findViewById(R.id.buttonCapture);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         drawer = findViewById(R.id.drawer_layout);
@@ -24,6 +56,34 @@ public class ActivityMenu extends AppCompatActivity {
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentCamera()).commit();
+            navigationView.setCheckedItem(R.id.nav_camera);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_camera:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentCamera()).commit();
+                break;
+            case R.id.nav_road:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentRoad()).commit();
+                break;
+            case R.id.nav_car:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentCar()).commit();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(this, "Settings!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_communication:
+                Toast.makeText(this, "Communication!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
@@ -32,6 +92,70 @@ public class ActivityMenu extends AppCompatActivity {
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+        }
+    }
+
+    public void onClickSend(View view) {
+        Toast.makeText(this, "onClickSend", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickCapture(View view) {
+        //for SKD older than 23
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permission, PERMISSION_CODE);
+            }
+            else {
+                //permissions already granted
+                openCamera();
+            }
+        }
+        else {
+            //SDK >= 23
+            openCamera();
+        }
+    }
+
+    private void openCamera() {
+        //Image information
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.TITLE, "New image");
+        contentValues.put(MediaStore.Images.Media.DESCRIPTION, "From the camera");
+        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+        //Camera intent
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //all permissions granted
+                    openCamera();
+                }
+                else {
+                    Toast.makeText(this, "Permissions denied...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //after taking picture from camera
+        if(resultCode == RESULT_OK) {
+            //Show taken picture in out imageViewCamera
+            imageViewCamera.setImageURI(imageUri);
         }
     }
 }
--- AndroidApp/app/src/main/java/com/example/cpsystemsapp/FragmentCamera.java
+++ AndroidApp/app/src/main/java/com/example/cpsystemsapp/FragmentCamera.java
@@ -1,18 +1,31 @@
 package com.example.cpsystemsapp;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FragmentCamera extends Fragment {
+    ImageView imageViewCamera;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_camera, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
 }
