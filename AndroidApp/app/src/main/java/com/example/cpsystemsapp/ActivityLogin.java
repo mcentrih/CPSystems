4aaebecd792bce335e89162255cd6a4169856025
Mcentrih <centrihmatjaz@gmail.com>
2021-06-08 13:55:08 +0200

    NPO
    - baza cesta

--- AndroidApp/app/src/main/java/com/example/cpsystemsapp/ActivityLogin.java
+++ AndroidApp/app/src/main/java/com/example/cpsystemsapp/ActivityLogin.java
@@ -16,7 +16,7 @@ import android.widget.Toast;
 import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class ActivityLogin extends AppCompatActivity {
-    String URLLogin = "http://192.168.1.11/CPSystems/podatkovnaBaza/login.php";
+    String URLLogin = "http://192.168.3.117/CPSystems/podatkovnaBaza/login.php";
 
     TextView notRegistered;
     Button btn_login;
