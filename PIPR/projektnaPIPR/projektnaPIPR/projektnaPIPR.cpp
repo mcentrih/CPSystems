2822a5fb1ea43b348175110938862777239680fb
Žan Plahuta <79518305+zanplahuta@users.noreply.github.com>
2022-01-09 00:48:53 +0100

    PIPR->deluje
    -sprejemanje in branje .json formata
    -delitev podatkov na dva enaka dela
    -racunanje obeh delov paralelno in porazdeljeno
    -sprejem obeh delov podatkov in zdruzitev

--- PIPR/projektnaPIPR/projektnaPIPR/projektnaPIPR.cpp
+++ PIPR/projektnaPIPR/projektnaPIPR/projektnaPIPR.cpp
@@ -5,32 +5,99 @@
 #include <mpi.h>
 #include <fstream>
 #include <json/value.h>
+#include <string>
+#include <json/json.h>
+#include <vector>
 
 using namespace std;
 
 vector<int> bumps;
 unsigned lineCounter = 0;
+vector<vector<int>> jsonVector;
+int jsonSIZE = 0;
+vector<int> vector1;
+vector<int> vector2;
 
-void sumFirstHalf(vector<int> vectorFirst, int first) {
-	int firstHalfSum = 0;
+void sumFirstHalf(vector<vector<int>> vectorFirst, int first) {
+	int vsota1 = 0;
+	vector<int> povprecja1;
+	int povprecje1 = 0;
 
-	for (int i = 0; i < first; i++) {
-		firstHalfSum = firstHalfSum + vectorFirst[i];
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < vectorFirst[i].size(); j++) {
+			vsota1 = vsota1 + vectorFirst[i][j];
+		}
+		povprecje1 = vsota1 / jsonVector[i].size();
+		povprecja1.push_back(povprecje1);
+		povprecje1 = 0;
+		vsota1 = 0;
 	}
-	cout << "First half SUM: " << firstHalfSum << endl;
-	MPI_Send(&firstHalfSum, 1, MPI_INT, 2, 0, MPI_COMM_WORLD);
+	int a = povprecja1.size();
+	MPI_Send(&a, 1, MPI_INT, 2, 1, MPI_COMM_WORLD);
+	MPI_Send(povprecja1.data(), a, MPI_INT, 2, 2, MPI_COMM_WORLD);
 
 }
 
-void sumSecondHalf(vector<int> vectorFirst, int second) {
-	int secondHalfSum = 0;
+void sumSecondHalf(vector<vector<int>> vectorFirst, int second) {
+	int vsota2 = 0;
+	vector<int> povprecja2;
+	int povprecje2 = 0;
 
-	for (int i = second; i < (second * 2); i++) {
-		secondHalfSum = secondHalfSum + vectorFirst[i];
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < vectorFirst[i].size(); j++) {
+			vsota2 = vsota2 + vectorFirst[i][j];
+		}
+		povprecje2 = vsota2 / jsonVector[i].size();
+		povprecja2.push_back(povprecje2);
+		povprecje2 = 0;
+		vsota2 = 0;
 	}
-	cout << "Second half SUM: " << secondHalfSum << endl;
-	MPI_Send(&secondHalfSum, 1, MPI_INT, 2, 0, MPI_COMM_WORLD);
+	int b = povprecja2.size();
+	MPI_Send(&b, 1, MPI_INT, 2, 3, MPI_COMM_WORLD);
+	MPI_Send(povprecja2.data(), b, MPI_INT, 2, 4, MPI_COMM_WORLD);
+}
 
+void preberiJSON(string file) {
+	ifstream ifFile(file.c_str());
+	Json::Value actualJson;
+	Json::Reader reader;
+
+	reader.parse(ifFile, actualJson);
+	jsonSIZE = actualJson["podatki"].size();
+	vector<int> rel1;
+	vector<int> rel2;
+	vector<int> rel3;
+	vector<int> rel4;
+	vector<int> rel5;
+	vector<int> rel6;
+
+	for (int i = 0; i < jsonSIZE; i++) {
+		if (actualJson["podatki"][i]["idRelacija"] == "1") {
+			rel1.push_back(actualJson["podatki"][i]["bumps"].asInt());
+		}
+		else if (actualJson["podatki"][i]["idRelacija"] == "2") {
+			rel2.push_back(actualJson["podatki"][i]["bumps"].asInt());
+		}
+		else if (actualJson["podatki"][i]["idRelacija"] == "3") {
+			rel3.push_back(actualJson["podatki"][i]["bumps"].asInt());
+		}
+		else if (actualJson["podatki"][i]["idRelacija"] == "4") {
+			rel4.push_back(actualJson["podatki"][i]["bumps"].asInt());
+		}
+		else if (actualJson["podatki"][i]["idRelacija"] == "5") {
+			rel5.push_back(actualJson["podatki"][i]["bumps"].asInt());
+		}
+		else if (actualJson["podatki"][i]["idRelacija"] == "6") {
+			rel6.push_back(actualJson["podatki"][i]["bumps"].asInt());
+		}
+	}
+
+	jsonVector.push_back(rel1);
+	jsonVector.push_back(rel2);
+	jsonVector.push_back(rel3);
+	jsonVector.push_back(rel4);
+	jsonVector.push_back(rel5);
+	jsonVector.push_back(rel6);
 }
 
 int countLinesInFile(string file) {
@@ -43,49 +110,22 @@ int countLinesInFile(string file) {
 		istream_iterator<char>(),
 		'\n');
 
-	//cout << "Lines: " << lineCounter<< "\n";
 	return lineCounter;
 }
 
 int main() {
 	MPI_Init(NULL, NULL);
-	string file = "test.txt";
+	string jsonFile = "prebrano.json";
 	int firstHalf = 0;
 	int secondHalf = 0;
-	int sumFirst = 0;
-	int sumSecond = 0;
-	int result = 0;
+	vector<int> done;
 
-	/*STEVILO VRSTIC V DATOTEKI*/
-	lineCounter = countLinesInFile(file.c_str());
-	//cout << "VRSTICE: " << lineCounter << endl;
+	/*BRANJE .json*/
+	preberiJSON(jsonFile);
 
-	/*BRANJE DATOTEKE*/
-	ifstream inFile;
-	inFile.open(file.c_str());
-	int trenutniBump = 0;
-
-	//BRANJE IN SHRANJEVANJE
-	if (inFile.is_open()) {
-		for (int i = 0; i < lineCounter + 1; i++) {
-			inFile >> trenutniBump;
-			bumps.push_back(trenutniBump);
-		}
-		inFile.close();
-	}
-
-	/*TESTNI IZPIS*/
-	//for (int i = 0; i < lineCounter + 1; i++) {
-	//	cout << bumps[i] << " ";
-	//}
-	//cout << endl;
-	/*---------------*/
-
-	firstHalf = lineCounter / 2;
-	secondHalf = lineCounter - firstHalf;
-	firstHalf++;
-	//cout << "FIRST: " << firstHalf << endl;
-	//cout << "SECOND: " << secondHalf << endl;
+	size_t const half_size = jsonVector.size() / 2;
+	vector<vector<int>> split_low(jsonVector.begin(), jsonVector.begin() + half_size);
+	vector<vector<int>> split_high(jsonVector.begin() + (half_size), jsonVector.end());
 
 	int my_rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
@@ -93,20 +133,29 @@ int main() {
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 	//prva polovica podatkov
 	if (my_rank == 0) {
-		sumFirstHalf(bumps, firstHalf);
+		sumFirstHalf(split_low, firstHalf);
 	}
 	//druga polovica podatkov
 	else if (my_rank == 1) {
-		sumSecondHalf(bumps, secondHalf);
+		sumSecondHalf(split_high, secondHalf);
 	}
 	if (my_rank == 2) {
-		MPI_Recv(&sumFirst, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
-			MPI_STATUS_IGNORE);
-		MPI_Recv(&sumSecond, 1, MPI_INT, 1, 0, MPI_COMM_WORLD,
-			MPI_STATUS_IGNORE);
-		result = sumFirst + sumSecond;
-		cout << "Rezultat: " << result << endl;	//skupno stevilo bumpov
+		int c;
+		int d;
+
+		MPI_Recv(&c, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		vector<int> result1(c);
+		MPI_Recv(result1.data(), c, MPI_INT, 0, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(&d, 1, MPI_INT, 1, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		vector<int> result2(d);
+		MPI_Recv(result2.data(), d, MPI_INT, 1, 4, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+		done.insert(done.end(), result1.begin(), result1.end());
+		done.insert(done.end(), result2.begin(), result2.end());
+
+		for (int i = 0; i < done.size(); i++) {
+			cout << "Povprecje destinacije " << i + 1 << "-> " << done[i] << endl;
+		}
 	}
 	MPI_Finalize();
-
 }
